8b66b1ba9891146b92a849d14bd5841463764a28
samuelfenton <samuel.fenton@outlook.com>
2020-08-01 15:41:57 +1000

    Added in neon, added in new art assets

--- Assets/Scripts/Shaders/CustomNeon.cs
+++ Assets/Scripts/Shaders/CustomNeon.cs
@@ -1,97 +1,233 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class CustomNeon : MonoBehaviour
 {
+    private const string COLOR_STRING = "_Color";
+    private const string EMISSION_STRING = "_EmissionColor";
+
     [System.Serializable]
-    public struct NEON_FLASH
+    public struct NEON_SECTION
     {
-        public Color m_color;
-        public float m_flashTime;
+        public Color m_neonColor;
+        public float m_timeCode;
     }
 
+    [System.Serializable]
+    public struct NEON_SEQUENCE_DATA
+    {
+        public GameObject m_neonObject;
+        public List<NEON_SECTION> m_neonSequence;
+
+        /// <summary>
+        /// Check if struct is full of valid data
+        /// </summary>
+        /// <returns>true when valid and usable</returns>
+        public bool IsValidData()
+        {
+            if (m_neonObject == null)
+            {
+#if UNITY_EDITOR
+                Debug.LogWarning("One or more of the sections has no designated neon gameobjects");
+#endif
+                return false;
+            }
+
+            if (m_neonSequence.Count == 0)
+            {
+#if UNITY_EDITOR
+                Debug.LogWarning(m_neonObject.name + " has no assigned sequence, and will be ignored");
+#endif
+                return false;
+            }
+
+            return true;
+        }
+    }
+
+    public class NeonSequence : MonoBehaviour
+    {
+        private GameObject m_neonObject = null;
+        private List<NEON_SECTION> m_neonSequence;
+
+        private int m_currentIndex = 0;
+        private Material[] m_materials = new Material[0];
+
+        private Coroutine m_loopCoroutine = null;
+
+        private string[] m_targetVariables;
+
+        /// <summary>
+        /// Initialise the seqeance
+        /// </summary>
+        /// <param name="p_validData">Data to copy over from</param>
+        /// <param name="p_targetVariables">String to use when modifying color</param>
+        /// <returns>True when inialisation has completed</returns>
+        public bool InitSequence(NEON_SEQUENCE_DATA p_validData, string[] p_targetVariables)
+        {
+            //Copy data
+            m_neonObject = p_validData.m_neonObject;
+            m_neonSequence = p_validData.m_neonSequence;
+            m_targetVariables = p_targetVariables;
+
+            //Valid setup
+            MeshRenderer[] sectionRenderers = m_neonObject.GetComponentsInChildren<MeshRenderer>();
+
+            if (sectionRenderers.Length == 0)
+            {
+#if UNITY_EDITOR
+                Debug.LogWarning(m_neonObject.name + " has no attached mesh renderers");
+#endif
+                return false;
+            }
+
+            //Setup material
+            m_materials = new Material[sectionRenderers.Length];
+
+            //Apply to all renderers
+            for (int rendererIndex = 0; rendererIndex < sectionRenderers.Length; rendererIndex++)
+            {
+                m_materials[rendererIndex] = Instantiate(sectionRenderers[rendererIndex].material);
+                m_materials[rendererIndex].EnableKeyword("_EMISSION");
+                sectionRenderers[rendererIndex].sharedMaterial = m_materials[rendererIndex];
+            }
+
+            return true;
+        }
+
+        /// <summary>
+        /// Reset neon sequence to first index
+        /// </summary>
+        public void ResetSequence()
+        {
+            m_currentIndex = 0;
+
+            if (m_loopCoroutine != null)
+            {
+                StopCoroutine(m_loopCoroutine);
+                m_loopCoroutine = null;
+            }
+
+            m_loopCoroutine = StartCoroutine(NextSection());
+        }
+
+        /// <summary>
+        /// Delay till next neon change
+        /// </summary>
+        private IEnumerator NextSection()
+        {
+            SetColour(m_neonSequence[m_currentIndex].m_neonColor);
+
+            if (m_currentIndex == m_neonSequence.Count - 1)
+            {
+                yield break;
+            }
+
+            float nextDelay = m_neonSequence[m_currentIndex + 1].m_timeCode - m_neonSequence[m_currentIndex].m_timeCode;
+
+            yield return new WaitForSeconds(Mathf.Max(0.0f, nextDelay));
+
+            m_currentIndex++;
+
+            m_loopCoroutine = StartCoroutine(NextSection());
+        }
+
+        /// <summary>
+        /// Set color for all materials being used
+        /// </summary>
+        /// <param name="p_color">Color to set to</param>
+        private void SetColour(Color p_color)
+        {
+            for (int materialIndex = 0; materialIndex < m_materials.Length; materialIndex++)
+            {
+                //Assign to all varibles
+                for (int varibleIndex = 0; varibleIndex < m_targetVariables.Length; varibleIndex++)
+                {
+                    m_materials[materialIndex].SetColor(m_targetVariables[varibleIndex], p_color);
+                }
+            }
+        }
+    }
+
+    [Header("Settings")]
     public float m_totalTime = 0.0f;
+    public enum NEON_TYPE {COLOR, EMISSION, BOTH}
+    [Tooltip("COLOR: Changes base color, used more for posters/prelit objects, EMISSION: Adds emission to object, used more on neon tube signs, BOTH: Add to both colour and emission, used more for LCD screens")]
+    public NEON_TYPE m_colourType = NEON_TYPE.COLOR;
 
-    public List<NEON_FLASH> m_neonSequence = new List<NEON_FLASH>();
+    [Header("Advanced")]
+    [Tooltip("Only change if youre not using the default varible names in the shader, otherwise leave empty")]
+    public string m_materialVarible = "";
 
-    public GameObject m_objectWithRenderer = null;
+    [Header("Sequence Data")]
+    public List<NEON_SEQUENCE_DATA> m_neonSequenceData = new List<NEON_SEQUENCE_DATA>();
+    private List<NeonSequence> m_neonSequence = new List<NeonSequence>();
 
-    private MeshRenderer m_meshRenderer = null;
-    private Material m_material = null;
-
+    /// <summary>
+    /// Setup all sequencing based off data given
+    /// </summary>
     private void Start()
     {
-        if(m_neonSequence.Count > 0)
+        string[] variableStrings = new string[0];
+
+        if(m_materialVarible != "") //Preassigned varible
         {
-            //Get varibles
-            if(m_objectWithRenderer == null)
-            {
-                m_meshRenderer = GetComponent<MeshRenderer>();
-
-                if (m_meshRenderer == null)
-                    m_meshRenderer = GetComponentInChildren<MeshRenderer>();
-            }
-            else
-            {
-                m_meshRenderer = m_objectWithRenderer.GetComponent<MeshRenderer>();
-
-                if (m_meshRenderer == null)
-                    m_meshRenderer = m_objectWithRenderer.GetComponentInChildren<MeshRenderer>();
-            }
-
-            if (m_meshRenderer == null) //Try children
-            {
-#if UNITY_EDITOR
-                Debug.LogWarning(name + ": Has no renderer on base object or assigned object, will check for any on its children");
-#endif
-                Destroy(this);
-                return;
-            }
-
-            if (m_meshRenderer == null) //Found nothing
-            {
-#if UNITY_EDITOR
-                Debug.LogError(name + ": No mesh renderer is found on the neon object or its children");
-#endif
-                Destroy(this);
-                return;
-            }
-
-            m_material = m_meshRenderer.material;
-
-            if (m_material == null)
-            {
-#if UNITY_EDITOR
-                Debug.LogError(name + ": No material is found on the neon object");
-#endif
-                Destroy(this);
-                return;
-            }
-
-            //Clone usage
-            m_material = Instantiate(m_material);
-            m_meshRenderer.material = m_material;
-            m_material.EnableKeyword("_EMISSION");
-
-            StartCoroutine(ChangeColor(0));
+            variableStrings = new string[1];
+            variableStrings[0] = m_materialVarible;
         }
+        else
+        {
+            switch (m_colourType)
+            {
+                case NEON_TYPE.COLOR:
+                case NEON_TYPE.EMISSION:
+                    variableStrings = new string[1];
+                    variableStrings[0] = m_colourType == NEON_TYPE.COLOR ? COLOR_STRING : EMISSION_STRING;
+                    break;
+                case NEON_TYPE.BOTH:
+                    variableStrings = new string[2];
+                    variableStrings[0] = COLOR_STRING;
+                    variableStrings[1] = EMISSION_STRING;
+                    break;
+            }
+        }
+            
+        for (int sequenceIndex = 0; sequenceIndex < m_neonSequenceData.Count; sequenceIndex++)
+        {
+            if(m_neonSequenceData[sequenceIndex].IsValidData())
+            {
+                NeonSequence nextSequence = gameObject.AddComponent<NeonSequence>();
+
+                if (nextSequence.InitSequence(m_neonSequenceData[sequenceIndex], variableStrings))
+                    m_neonSequence.Add(nextSequence);
+                else
+                    Destroy(nextSequence);
+            }
+        }
+
+        if(m_neonSequence.Count == 0)
+        {
+            return;
+        }
+
+        StartCoroutine(TotalSequence());
     }
 
-    private IEnumerator ChangeColor(int p_currentIndex)
+    /// <summary>
+    /// Run first neon sequence for each, then delay till loop ends
+    /// </summary>
+    /// <returns></returns>
+    private IEnumerator TotalSequence()
     {
-        m_material.SetColor("_EmissionColor", m_neonSequence[p_currentIndex].m_color);
-
-        float nextTime = p_currentIndex == m_neonSequence.Count -1 ? m_totalTime - m_neonSequence[p_currentIndex].m_flashTime : m_neonSequence[p_currentIndex + 1].m_flashTime - m_neonSequence[p_currentIndex].m_flashTime;
-
-        yield return new WaitForSeconds(Mathf.Max(nextTime, 0.0f, nextTime));
-
-        p_currentIndex += 1;
-        if (p_currentIndex >= m_neonSequence.Count)
+        for (int sequenceIndex = 0; sequenceIndex < m_neonSequence.Count; sequenceIndex++)
         {
-            p_currentIndex = 0;
+            m_neonSequence[sequenceIndex].ResetSequence();
         }
 
-        StartCoroutine(ChangeColor(p_currentIndex));
+        yield return new WaitForSeconds(m_totalTime);
+
+        StartCoroutine(TotalSequence());
     }
 }
