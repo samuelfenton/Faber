31b5c8c6cd1b2f6c0bd375ed1d91f54dcc5463bc
samuelfenton <samuel.fenton@outlook.com>
2020-08-09 23:42:09 +1000

    File reorganisation to ne new naming convention

--- Assets/Scripts/Entity/Character/NPC/NPC_Flyer_Drone.cs
+++ Assets/Scripts/Entity/Character/NPC/NPC_Flyer_Drone.cs
@@ -8,6 +8,7 @@ public class NPC_Flyer_Drone : NPC_Flyer
     public GameObject m_hoverComponentObject = null;
     public GameObject m_weaponObject = null;
 
+    public float m_degreesOfTilt = 30.0f;
 
     /// <summary>
     /// Initiliase the entity
