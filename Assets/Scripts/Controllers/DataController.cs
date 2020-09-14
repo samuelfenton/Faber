919b0b78393f370a175d8f95c5177fa399d94f34
samuelfenton <samuel.fenton@outlook.com>
2020-09-14 11:55:24 +1000

    Added in saving of player prefs for options

--- Assets/Scripts/Controllers/DataController.cs
+++ Assets/Scripts/Controllers/DataController.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class DataController
 {
+    public enum PLAYER_PREF_VARIABLES {MASTER_VOLUME, MUSIC_VOLUME, SFX_VOLUME, VOICE_VOLUME, SUBTITLES, PERFORMANCE_INDEX, RESOLUTION_INDEX, CUSTOM_RESOLUTION_X, CUSTOM_RESOLUTION_Y}
+
+    public static string[] m_prefsToStrings = new string[] { "Master_Volume", "Music_Volume", "SFX_Volume", "Voice_Volume", "Subtitles", "Performance_Index", "Resolution_Index", "Custom_Resolution_X", "Custom_Resolution_Y" };
+
     /// <summary>
     /// Initialise the data used in game. 
     /// </summary>
@@ -11,4 +15,56 @@ public class DataController
     {
 
     }
+
+    /// <summary>
+    /// Fill struct with saved data if available
+    /// </summary>
+    /// <returns>Saved data, or default struct data when not previously saved</returns>
+    public static UIController_MainMenu.OptionVariables GetOptionsData()
+    {
+        UIController_MainMenu.OptionVariables optionVariables = new UIController_MainMenu.OptionVariables();
+
+        if (PlayerPrefs.HasKey(m_prefsToStrings[(int)PLAYER_PREF_VARIABLES.MASTER_VOLUME]))
+            optionVariables.m_masterVolume = PlayerPrefs.GetInt(m_prefsToStrings[(int)PLAYER_PREF_VARIABLES.MASTER_VOLUME]);
+        if (PlayerPrefs.HasKey(m_prefsToStrings[(int)PLAYER_PREF_VARIABLES.MUSIC_VOLUME]))
+            optionVariables.m_musicVolume = PlayerPrefs.GetInt(m_prefsToStrings[(int)PLAYER_PREF_VARIABLES.MUSIC_VOLUME]);
+        if (PlayerPrefs.HasKey(m_prefsToStrings[(int)PLAYER_PREF_VARIABLES.SFX_VOLUME]))
+            optionVariables.m_SFXVolume = PlayerPrefs.GetInt(m_prefsToStrings[(int)PLAYER_PREF_VARIABLES.SFX_VOLUME]);
+        if (PlayerPrefs.HasKey(m_prefsToStrings[(int)PLAYER_PREF_VARIABLES.VOICE_VOLUME]))
+            optionVariables.m_voiceVolume = PlayerPrefs.GetInt(m_prefsToStrings[(int)PLAYER_PREF_VARIABLES.VOICE_VOLUME]);
+
+        if (PlayerPrefs.HasKey(m_prefsToStrings[(int)PLAYER_PREF_VARIABLES.SUBTITLES]))
+            optionVariables.m_subtitles = PlayerPrefs.GetInt(m_prefsToStrings[(int)PLAYER_PREF_VARIABLES.SUBTITLES]) == 1;
+
+        if (PlayerPrefs.HasKey(m_prefsToStrings[(int)PLAYER_PREF_VARIABLES.PERFORMANCE_INDEX]))
+            optionVariables.m_performanceIndex = PlayerPrefs.GetInt(m_prefsToStrings[(int)PLAYER_PREF_VARIABLES.PERFORMANCE_INDEX]);
+        if (PlayerPrefs.HasKey(m_prefsToStrings[(int)PLAYER_PREF_VARIABLES.RESOLUTION_INDEX]))
+            optionVariables.m_resolutionIndex = PlayerPrefs.GetInt(m_prefsToStrings[(int)PLAYER_PREF_VARIABLES.RESOLUTION_INDEX]);
+        if (PlayerPrefs.HasKey(m_prefsToStrings[(int)PLAYER_PREF_VARIABLES.CUSTOM_RESOLUTION_X]))
+            optionVariables.m_customResolutionX = PlayerPrefs.GetInt(m_prefsToStrings[(int)PLAYER_PREF_VARIABLES.CUSTOM_RESOLUTION_X]);
+        if (PlayerPrefs.HasKey(m_prefsToStrings[(int)PLAYER_PREF_VARIABLES.CUSTOM_RESOLUTION_Y]))
+            optionVariables.m_customResolutionY = PlayerPrefs.GetInt(m_prefsToStrings[(int)PLAYER_PREF_VARIABLES.CUSTOM_RESOLUTION_Y]);
+
+        return optionVariables;
+    }
+
+    /// <summary>
+    /// Save the given structs data into player prefs 
+    /// </summary>
+    /// <param name="p_data">No data will be verified, this should be done previously</param>
+    public static void SaveOptionsData(UIController_MainMenu.OptionVariables p_data)
+    {
+        PlayerPrefs.SetInt(m_prefsToStrings[(int)PLAYER_PREF_VARIABLES.MASTER_VOLUME], p_data.m_masterVolume);
+        PlayerPrefs.SetInt(m_prefsToStrings[(int)PLAYER_PREF_VARIABLES.MUSIC_VOLUME], p_data.m_musicVolume);
+        PlayerPrefs.SetInt(m_prefsToStrings[(int)PLAYER_PREF_VARIABLES.SFX_VOLUME], p_data.m_SFXVolume);
+        PlayerPrefs.SetInt(m_prefsToStrings[(int)PLAYER_PREF_VARIABLES.VOICE_VOLUME], p_data.m_voiceVolume);
+        PlayerPrefs.SetInt(m_prefsToStrings[(int)PLAYER_PREF_VARIABLES.SUBTITLES], p_data.m_subtitles ? 1 : 0);
+        PlayerPrefs.SetInt(m_prefsToStrings[(int)PLAYER_PREF_VARIABLES.PERFORMANCE_INDEX], p_data.m_performanceIndex);
+        PlayerPrefs.SetInt(m_prefsToStrings[(int)PLAYER_PREF_VARIABLES.RESOLUTION_INDEX], p_data.m_resolutionIndex);
+        PlayerPrefs.SetInt(m_prefsToStrings[(int)PLAYER_PREF_VARIABLES.CUSTOM_RESOLUTION_X], p_data.m_customResolutionX);
+        PlayerPrefs.SetInt(m_prefsToStrings[(int)PLAYER_PREF_VARIABLES.CUSTOM_RESOLUTION_Y], p_data.m_customResolutionY);
+
+        PlayerPrefs.Save();
+    }
+
 }
--- Assets/Scripts/Controllers/Main Menu/UIController_MainMenu.cs
+++ Assets/Scripts/Controllers/Main Menu/UIController_MainMenu.cs
@@ -1,17 +1,75 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class UIController_MainMenu : UIController
 {
+    public struct OptionVariables
+    {
+        public OptionVariables(int p_masterVolume = 100, int p_musicVolume = 100, int p_SFXVolume = 100, int p_voiceVolume = 100, bool p_subtitles = true, int p_performanceIndex = 2, int p_resolutionIndex = 3, int p_customResolutionX = 640, int p_customResolutionY = 640)
+        {
+            m_masterVolume = p_masterVolume;
+            m_musicVolume = p_musicVolume;
+            m_SFXVolume = p_SFXVolume;
+            m_voiceVolume = p_voiceVolume;
+
+            m_subtitles = p_subtitles;
+
+            m_performanceIndex = p_performanceIndex;
+            m_resolutionIndex = p_resolutionIndex;
+            m_customResolutionX = p_customResolutionX;
+            m_customResolutionY = p_customResolutionY;
+        }
+        public int m_masterVolume;
+        public int m_musicVolume;
+        public int m_SFXVolume;
+        public int m_voiceVolume;
+
+        public bool m_subtitles;
+
+        public int m_performanceIndex;
+        
+        public int m_resolutionIndex;
+        public int m_customResolutionX;
+        public int m_customResolutionY;
+    }
+
+    public OptionVariables m_optionsVariables;
+
+
+    private const int CUSTOM_RESOLUTION_INDEX = 13;
+    private const int MIN_RESOLUTION_VAL = 640;
+    private const int MAX_RESOLUTION_VAL = 7680;
+
+    private static Color INTERACTIVE_UI_COLOR = Color.white;
+    private static Color NON_INTERACTIVE_UI_COLOR = new Color(0.4f, 0.4f, 0.4f, 1.0f);
+
     [Header("Assigned Variables")]
     public GameObject m_UIObjectMainMenu = null;
     public GameObject m_UIObjectOptions = null;
     public GameObject m_UIObjectCredits = null;
 
+    [Header("Options Variables")]
+    public Slider m_masterVolumeSlider = null;
+    public Slider m_musicVolumeSlider = null;
+    public Slider m_SFXVolumeSlider = null;
+    public Slider m_voiceVolumeSlider = null;
+
+    public Toggle m_subtitleToggle = null;
+
+    public TMP_Dropdown m_performanceDropdown = null;
+    public TMP_Dropdown m_resolutionDropdown = null;
+
+    public TMP_InputField m_customResXInput = null;
+    public TMP_InputField m_customResYInput = null;
+
+    public TextMeshProUGUI m_customResXLabel = null;
+    public TextMeshProUGUI m_customResYLabel = null;
+
     private SceneController_MainMenu m_mainMenuSceneController = null;
-    private enum MAINMENU_STATE {MAINMENU, OPTIONS, CREDITS}
-    private MAINMENU_STATE m_currentState = MAINMENU_STATE.MAINMENU;
+
     /// <summary>
     /// Setup variables to be used in UI
     /// </summary>
@@ -20,7 +78,7 @@ public class UIController_MainMenu : UIController
         base.Init();
 
         m_mainMenuSceneController = (SceneController_MainMenu)(MasterController.Instance.m_currentSceneController);
-        
+
         if (m_mainMenuSceneController == null)
         {
 #if UNITY_EDITOR || DEVELOPMENT_BUILD 
@@ -39,12 +97,108 @@ public class UIController_MainMenu : UIController
             return;
         }
 
-        m_currentState = MAINMENU_STATE.MAINMENU;
+        if (m_masterVolumeSlider == null || m_musicVolumeSlider == null || m_SFXVolumeSlider == null || m_voiceVolumeSlider == null ||
+            m_subtitleToggle == null || 
+            m_performanceDropdown == null || m_resolutionDropdown == null || m_customResXInput == null || m_customResYInput == null || 
+            m_customResXLabel == null || m_customResYLabel == null)
+        {
+#if UNITY_EDITOR || DEVELOPMENT_BUILD 
+            Debug.LogWarning(name + " does not have all its required options variables assigned");
+#endif
+            Destroy(gameObject);
+            return;
+        }
+
+        //Fill data
+        m_optionsVariables = DataController.GetOptionsData();
+        RebuildOptionUIVariables();
 
         m_UIObjectMainMenu.SetActive(true);
         m_UIObjectOptions.SetActive(false);
         m_UIObjectCredits.SetActive(false);
+
+        //Call in start to set defaults after loading data
+        OnChange_Resolution();
+        OnChange_CustomResolution();
     }
+
+    /// <summary>
+    /// Apply the struct variables to the vairbels found in the UI
+    /// </summary>
+    public void RebuildOptionUIVariables()
+    {
+        m_masterVolumeSlider.value = m_optionsVariables.m_masterVolume;
+        m_musicVolumeSlider.value = m_optionsVariables.m_musicVolume;
+        m_SFXVolumeSlider.value = m_optionsVariables.m_SFXVolume;
+        m_voiceVolumeSlider.value = m_optionsVariables.m_voiceVolume;
+
+        m_subtitleToggle.isOn = m_optionsVariables.m_subtitles;
+
+        m_performanceDropdown.value = m_optionsVariables.m_performanceIndex;
+        m_resolutionDropdown.value = m_optionsVariables.m_resolutionIndex;
+        m_customResXInput.text = m_optionsVariables.m_customResolutionX.ToString();
+        m_customResYInput.text = m_optionsVariables.m_customResolutionY.ToString();
+    }
+
+    #region Option on change
+
+    /// <summary>
+    /// Called when resoulition dorpdown has had a change in value
+    /// </summary>
+    public void OnChange_Resolution()
+    {
+        if(m_resolutionDropdown.value == CUSTOM_RESOLUTION_INDEX)
+        {
+            m_customResXInput.interactable = true;
+            m_customResYInput.interactable = true;
+
+            m_customResXLabel.color = INTERACTIVE_UI_COLOR;
+            m_customResYLabel.color = INTERACTIVE_UI_COLOR;
+        }
+        else
+        {
+            m_customResXInput.interactable = false;
+            m_customResYInput.interactable = false;
+
+            m_customResXLabel.color = NON_INTERACTIVE_UI_COLOR;
+            m_customResYLabel.color = NON_INTERACTIVE_UI_COLOR;
+        }
+    }
+
+    /// <summary>
+    /// Called when custom resolution is deslected
+    /// Not on change as values are "snapped" to be valid
+    /// </summary>
+    public void OnChange_CustomResolution()
+    {
+        m_customResXInput.text = ValidateCustomResolution(m_customResXInput).ToString();
+        m_customResYInput.text = ValidateCustomResolution(m_customResYInput).ToString();
+    }
+
+    /// <summary>
+    /// Keep text value restricted between min and max resolution values
+    /// </summary>
+    /// <param name="p_textVal">Input field to check with be set to only accept int inputs</param>
+    /// <returns>A valid resolution</returns>
+    private int ValidateCustomResolution(TMP_InputField p_textVal)
+    {
+        if (int.TryParse(p_textVal.text, out int textVal))
+        {
+            if (textVal > MAX_RESOLUTION_VAL)
+                textVal = MAX_RESOLUTION_VAL;
+
+            else if (textVal < MIN_RESOLUTION_VAL)
+                textVal = MIN_RESOLUTION_VAL;
+
+            return textVal;
+        }
+
+        return MIN_RESOLUTION_VAL;
+    }
+
+    #endregion
+
+    #region Main menu Buttons
 
     /// <summary>
     /// Button to load game
@@ -88,7 +242,9 @@ public class UIController_MainMenu : UIController
     {
         Application.Quit();
     }
+    #endregion
 
+    #region Generic buttons
     /// <summary>
     /// Swap canvas to show main menu
     /// TODO, add in animaiton effect, fade in/slide in etc
@@ -99,4 +255,43 @@ public class UIController_MainMenu : UIController
         m_UIObjectOptions.SetActive(false);
         m_UIObjectCredits.SetActive(false);
     }
+
+    #endregion
+
+    #region Option Buttons
+
+    /// <summary>
+    /// Discard all changes made
+    /// </summary>
+    public void Btn_RevertChanges()
+    {
+        RebuildOptionUIVariables();
+
+        Btn_ReturnToMainMenu();
+    }
+
+    /// <summary>
+    /// Apply the changes made
+    /// Save new varibles to player prefs
+    /// </summary>
+    public void Btn_ApplyChanges()
+    {
+        //Apply changes to stuct
+        m_optionsVariables.m_masterVolume = Mathf.FloorToInt(m_masterVolumeSlider.value);
+        m_optionsVariables.m_musicVolume = Mathf.FloorToInt(m_musicVolumeSlider.value);
+        m_optionsVariables.m_SFXVolume = Mathf.FloorToInt(m_SFXVolumeSlider.value);
+        m_optionsVariables.m_voiceVolume = Mathf.FloorToInt(m_voiceVolumeSlider.value);
+        m_optionsVariables.m_subtitles = m_subtitleToggle;
+        m_optionsVariables.m_performanceIndex = m_performanceDropdown.value;
+        m_optionsVariables.m_resolutionIndex = m_resolutionDropdown.value;
+        m_optionsVariables.m_customResolutionX = ValidateCustomResolution(m_customResXInput);
+        m_optionsVariables.m_customResolutionY = ValidateCustomResolution(m_customResYInput);
+
+        //Save
+        DataController.SaveOptionsData(m_optionsVariables);
+
+        Btn_ReturnToMainMenu();
+    }
+
+    #endregion
 }
